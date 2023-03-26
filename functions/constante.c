d209e026df5740387becc7f9618c5adcb69f889d
Blazer25 <joaovitor.eugenio15@gmail.com>
2023-03-26 19:16:33 -0300

    feat(constante): Desenvolvido código para a função constante.

--- functions/constante.c
+++ functions/constante.c
@@ -1,2 +1,28 @@
-//Função constante ...
-//
+#include <stdio.h>
+#include <stdlib.h>
+
+float valorX = 0, valorK = 0;
+
+int main()
+{
+    printf("Digite o valor de X: \n");
+    while (scanf("%f", &valorX) != 1)
+    {
+        printf("Entrada inválida, digite um número real\n");
+        while (getchar() != '\n')
+            ;
+    }
+
+    printf("Digite o valor de K: \n");
+    while (scanf("%f", &valorK) != 1)
+    {
+        printf("Entrada inválida, digite um número real\n");
+        while (getchar() != '\n')
+            ;
+    }
+
+    printf("\n");
+    printf("O valor da função f(x) = %.4f é: \n", valorK);
+    printf("f(%.4f) = %.4f", valorX, valorK);
+    return 0;
+}
