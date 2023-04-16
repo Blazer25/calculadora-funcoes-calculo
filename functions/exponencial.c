1b1a35a18cf4bd1c1b5971fe5132e1a004692d7a
João <joaodetore7@gmail.com>
2023-04-16 15:30:29 -0300

    feat(exponencial): Desenvolvimento da função exponencial

--- functions/exponencial.c
+++ functions/exponencial.c
@@ -3,29 +3,29 @@
 #include <stdlib.h>
 
 /*Função exponencial*/
-float exponencial(float k, float x) { return pow(k, x); }
+float exponencial(float valorK, float valorX) { return pow(valorK, valorX); }
 
 int main() {
-  float k, x;
+  float valorK, valorX;
     /*Entradas*/
     do {
       printf("Digite o valor de k:\n");
-      scanf("%f", &k);
-      if (k <= 0 || k == 1) printf("Dígito inválido, tente novamente\n");
-    } while (k <= 0 || k == 1);
+      scanf("%f", &valorK);
+      if (valorK <= 0 || valorK == 1) printf("Dígito inválido, tente novamente\n");
+    } while (valorK <= 0 || valorK == 1);
     do {
       printf("Digite o valor de x:\n");
-      scanf("%f", &x);
-      if (x == 1 || x == 0) printf("Dígito inválido, tente novamente\n");
-    } while (x == 1 || x==0);
+      scanf("%f", &valorX);
+      if (valorX == 1 || valorX == 0) printf("Dígito inválido, tente novamente\n");
+    } while (valorX == 1 || valorX==0);
 
     /*Saidas*/
-    if (k > 1) {
+    if (valorK > 1) {
       printf("A curva é crescente.\n");
     } else {
       printf("A curva é decrescente.\n");
     }
-    printf("f(x) eh: %.4f\n", exponencial(k, x));
+    printf("f(x) eh: %.4f\n", exponencial(valorK, valorX));
 
   return 0;
 }
