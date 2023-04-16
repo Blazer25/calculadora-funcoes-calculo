8a1ca14b7691f43336b76d468537b3aad678ec2c
Giovani Murakami <giovani.murakami@outlook.com>
2023-04-16 16:33:11 -0300

    feat(seno): Desenvolvimento da função seno

---
+++ functions/seno.c
@@ -0,0 +1,47 @@
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/*Função seno graus*/
+float sinGraus(float valorX) { return sin(valorX * M_PI / 180); }
+/*Função seno radianos*/
+float sinRadianos(float valorX) { return sin(valorX * M_PI); }
+
+int main() {
+  float valorX;
+  int escolha;
+
+  /*Menu inicial*/
+  printf("Escolha o tipo do valor para utilizar:\n");
+  printf("1 Entrada em Graus\n");
+  printf("2 Entrada em radianos\n");
+  scanf("%d", &escolha);
+
+  switch (escolha) {
+  /*Graus*/
+  case 1:
+  printf("Digite o valor de x para calcular:\n");
+  while (scanf("%f", &valorX) != 1) {
+  printf("Entrada inválida, digite um número real\n");
+  while (getchar() != '\n');
+  }
+  printf("O valor de sin(%.4f graus) eh %.4f\n", valorX, sinGraus(valorX));
+  break;
+
+  /*Radianos*/
+  case 2:
+  printf("Digite o valor de x para calcular:\n");
+  while (scanf("%f", &valorX) != 1) {
+  printf("Entrada inválida, digite um número real\n");
+  while (getchar() != '\n');
+  }
+  printf("O valor de sin(%.4f radianos) eh %.4f\n", valorX, sinRadianos(valorX));
+  break;
+
+  default:
+  printf("Digite um número entre 1 e 2!!!");
+  return 1;
+  }
+  
+  return 0;
+}
