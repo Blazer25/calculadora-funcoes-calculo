6b53ae547466b12db92a1dba0f89fa7efe6f8d5d
João <joaodetore7@gmail.com>
2023-04-16 16:01:48 -0300

    feat(coseno): Desenvolvimento da função coseno

--- functions/coseno.c
+++ functions/coseno.c
@@ -10,34 +10,43 @@ float cosRadianos(float valorX) { return cos(valorX * M_PI); }
 int main() {
   float valorX;
   int escolha;
- 
-/*Menu inicial*/
-    printf("Escolha o tipo do valor para utilizar:\n");
-    printf("1 Entrada em Graus\n");
-    printf("2 Entrada em radianos\n");
-    scanf("%d", &escolha);
 
-    switch (escolha) {
-    /*Graus*/
-    case 1:
-      do {
-        printf("Digite o valor de x para calcular:\n");
-        scanf("%f", &valorX);
-        if (valorX < 0) printf("Dígito inválido, tente novamente\n");
-      } while (valorX < 0);
-      printf("O valor de cos(%.4f graus) eh %.4f\n", valorX, cosGraus(valorX));
-      break;
+  /*Menu inicial*/
+  printf("Escolha o tipo do valor para utilizar:\n");
+  printf("1 Entrada em Graus\n");
+  printf("2 Entrada em radianos\n");
+  scanf("%d", &escolha);
 
-    /*Radianos*/
-    case 2:
-      do {
-        printf("Digite o valor de x para calcular:\n");
-        scanf("%f", &valorX);
-        if (valorX < 0) printf("Dígito inválido, tente novamente\n");
-      } while (valorX < 0);
-      printf("O valor de cos(%.4f radianos) eh %.4f\n", valorX, cosRadianos(valorX));
-      break;
-    }
-    
+  switch (escolha) {
+  /*Graus*/
+  case 1:
+    do {
+      printf("Digite o valor de x para calcular:\n");
+      while (scanf("%f", &valorX) != 1) {
+        printf("Entrada inválida, digite um número real\n");
+        while (getchar() != '\n');
+      }
+      if (valorX < 0)
+        printf("Entrada inválida, digite um número real\n");
+    } while (valorX < 0);
+    printf("O valor de cos(%.4f graus) eh %.4f\n", valorX, cosGraus(valorX));
+    break;
+
+  /*Radianos*/
+  case 2:
+    do {
+      printf("Digite o valor de x para calcular:\n");
+      while (scanf("%f", &valorX) != 1) {
+        printf("Entrada inválida, digite um número real\n");
+        while (getchar() != '\n');
+      }
+      if (valorX < 0)
+        printf("Entrada inválida, digite um número real\n");
+    } while (valorX < 0);
+    printf("O valor de cos(%.4f radianos) eh %.4f\n", valorX,
+           cosRadianos(valorX));
+    break;
+  }
+  
   return 0;
 }
