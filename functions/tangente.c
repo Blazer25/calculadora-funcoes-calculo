19a49b1214d860803b91513f4a414d9505aac57f
CrysFS <crystianschlemper@gmail.com>
2023-04-22 15:14:23 -0300

    feat: implementação funcao tangente

--- functions/tangente.c
+++ functions/tangente.c
@@ -2,10 +2,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/*Função seno graus*/
+/*Função tangente graus*/
 float tanGraus(float valorX) { return tan(valorX * M_PI / 180); }
-/*Função seno radianos*/
-float tanRadianos(float valorX) { return tan(valorX * M_PI); }
+/*Função tangente radianos*/
+float tanRadianos(float valorX) { return tan(valorX); }
 
 int main() {
   float valorX;
@@ -20,31 +20,28 @@ int main() {
   switch (escolha) {
   /*Graus*/
   case 1:
-  printf("Digite o valor de x para calcular:\n");
-  while (scanf("%f", &valorX) != 1) {
-  printf("Entrada inválida, digite um número real\n");
-  while (getchar() != '\n');
-  }
-  printf("O valor de sin(%.4f graus) eh %.4f\n", valorX, sinGraus(valorX));
+    printf("Digite o valor de x para calcular:\n");
+    while (scanf("%f", &valorX) != 1 || valorX == 90 || (int)valorX % 180 == 90) {
+      printf("Entrada inválida, digite um número real e diferente de 90 e 270 (e seus multiplos)\n");
+      while (getchar() != '\n');
+    }
+    printf("O valor de tan(%.4f graus) eh %.4f\n", valorX, tanGraus(valorX));
   break;
 
   /*Radianos*/
   case 2:
-  printf("Digite o valor de x para calcular:\n");
-  while (scanf("%f", &valorX) != 1) {
-  printf("Entrada inválida, digite um número real\n");
-  while (getchar() != '\n');
-  }
-  printf("O valor de sin(%.4f radianos) eh %.4f\n", valorX, sinRadianos(valorX));
+    printf("Digite o valor de x para calcular:\n");
+    while (scanf("%f", &valorX) != 1 || valorX == (M_PI / 2) || valorX == (2 * M_PI / 3)) {
+      printf("Entrada inválida, digite um número real e diferente de pi/2 e 3pi/2\n");
+      while (getchar() != '\n');
+    }
+    printf("O valor de tan(%.4f radianos) eh %.4f\n", valorX, tanRadianos(valorX));
   break;
 
   default:
-  printf("Digite um número entre 1 e 2!!!");
-  return 1;
+    printf("Digite um número entre 1 e 2!!!");
+    return 1;
   }
   
   return 0;
 }
-
-//if (fmod(valor, M_PI / 2.0) == 0 && valor != M_PI / 2.0 && valor != 3 * M_PI / 2.0) {
-    //printf("Entrada invalida: tangente de 90 graus ou 270 graus.\n");
